850afb15c512f08da9f48a2bb186cd0d0e079265
ivaylotsachev <itsa4ev@gmail.com>
2019-06-11 20:54:42 +0300

    Initial commit

---
+++ src/components/JokeList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class JokeList extends Component {
+  async componentDidMount() {}
+
+  render() {
+    return <div>Jokes</div>;
+  }
+}
+
+export default JokeList;
