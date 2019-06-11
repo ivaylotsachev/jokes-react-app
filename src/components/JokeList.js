dfc9a27110ac01540eddcbe857d8382a78b22184
ivaylotsachev <itsa4ev@gmail.com>
2019-06-11 21:39:27 +0300

    Style container

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./JokeList.css";
 
 class JokeList extends Component {
   static defaultProps = {
@@ -33,7 +34,16 @@ class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Jokes</h1>
+        <div className="Jokelist_sidebar">
+          <h1 className="Jokelist_title">
+            <span>Funny</span> Jokes
+          </h1>
+          <img
+            src="https://cdn0.iconfinder.com/data/icons/classic-icons/512/093.png"
+            alt="app icon here"
+          />
+          <button className="Jokelist_getmore">Get more</button>
+        </div>
         <div className="JokeList_jokes">
           {this.state.jokes.map(j => (
             <div>{j}</div>
