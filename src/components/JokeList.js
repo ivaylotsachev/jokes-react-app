000609bfa143506ccba2b87e583e4693cdaa4a7d
ivaylotsachev <itsa4ev@gmail.com>
2019-06-11 21:05:02 +0300

    Fetch jokes

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -2,10 +2,45 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class JokeList extends Component {
-  async componentDidMount() {}
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = { jokes: [] };
+  }
+
+  async componentDidMount() {
+    /* load jokes */
+    let jokes = [];
+
+    while (jokes.length < this.props.numJokesToGet) {
+      let response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json"
+        }
+      });
+
+      jokes.push(response.data.joke);
+    }
+
+    console.log(jokes);
+    this.setState({ jokes });
+  }
 
   render() {
-    return <div>Jokes</div>;
+    return (
+      <div className="JokeList">
+        <h1>Jokes</h1>
+        <div className="JokeList_jokes">
+          {this.state.jokes.map(j => (
+            <div>{j}</div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
