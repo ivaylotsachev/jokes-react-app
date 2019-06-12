89d8f1024852884f7ee7eec4c38ea07fc2b5f1c5
ivaylotsachev <itsa4ev@gmail.com>
2019-06-12 20:36:02 +0300

    Updates

---
+++ precache-manifest.ad39b8febc2c4da8ee1fd96b98307e48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f18ccf8a79669805e9a128620be7025",
+    "url": "/jokes-react-app/index.html"
+  },
+  {
+    "revision": "df06131811287afcd922",
+    "url": "/jokes-react-app/static/css/main.e3f5d1b1.chunk.css"
+  },
+  {
+    "revision": "2ead9b1ffcbe4d1f895b",
+    "url": "/jokes-react-app/static/js/2.b1acdc21.chunk.js"
+  },
+  {
+    "revision": "df06131811287afcd922",
+    "url": "/jokes-react-app/static/js/main.d8a57275.chunk.js"
+  },
+  {
+    "revision": "4028387d501104b77fc2",
+    "url": "/jokes-react-app/static/js/runtime~main.f0655e4d.js"
+  }
+]);
